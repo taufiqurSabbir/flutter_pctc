8e2a9ec6508e90d85c87f7c15eee1c725461643b
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-06-11 22:43:51 +0600

    button,textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-//continer,Button,Icon,image
 
 void main() {
 runApp(myApp());
 }
+
 
 
 class myApp extends StatelessWidget {
@@ -12,82 +12,149 @@ class myApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'app pctc',
-      home:Homescreen(),
+    return MaterialApp(
+      title: 'button',
+      home: homeScreen(),
     );
   }
 }
 
 
-class Homescreen extends StatelessWidget {
-  const Homescreen({Key? key}) : super(key: key);
+class homeScreen extends StatelessWidget {
+  const homeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text('flutter pctc -2'),
+        title: Text('Button Pctc'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              width: 150,
-              color: Colors.deepPurple,
+      floatingActionButton:  FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-              alignment: Alignment.center,
-              child: Text('Hello bobo',
-              style: TextStyle(
-                  color: Colors.grey[200],
-                  fontSize: 25,
-
-              ),
-              ),
-            ),
-
-            Container(
-              height: 100,
-              width: 150,
-
-              margin:EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.green,
-               border: Border.all(color: Colors.grey, width: 4,),
-                borderRadius: BorderRadius.circular(20)
-              ),
-
-              alignment: Alignment.center,
-              child: Text('Hello bobo',
-                style: TextStyle(
-                  color: Colors.grey[200],
-                  fontSize: 25,
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    minimumSize: Size(150, 50),
+                    elevation: 5,
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                    shadowColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.redAccent),
+                    )
+                  ),
+                  onPressed: (){
+                    print('hello bobo');
+                  }, child: Text('Elevated Button')),
+              
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                  padding: EdgeInsets.all(15),
 
                 ),
+                  onPressed: (){},
+                  child: Text('text Button',style: TextStyle(color: Colors.white),)
               ),
-            ),
-            Icon(Icons.home,
-              color: Colors.deepPurple,
-              size: 32,
-              ),
-            Image.asset('asset/images/Thumbnail.png'),
 
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.deepPurple,
-                padding: EdgeInsets.symmetric(horizontal: 150,vertical: 30)
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.green)
+                ),
+                  onPressed: (){},
+                  child: Text('Outline Button')),
+
+              IconButton(
+                style: IconButton.styleFrom(
+                  side: BorderSide(color: Colors.red),
+                  padding: EdgeInsets.all(20)
+                ),
+                  onPressed: (){},
+                  icon: Icon(Icons.add)),
+
+              GestureDetector(
+                onDoubleTap: (){
+                  print('duble clicked');
+                },
+                  onLongPress: (){
+                    print('long clicked');
+                  },
+                onTap: (){
+                  print('photo clicked');
+                },
+                 child: Image.asset('asset/images/Thumbnail.png',width: 250,
+                  )
               ),
-                onPressed: (){
-              print('hello button click');
-            }, child: Text('click here',
-              style: TextStyle(fontSize: 20,
-                      fontWeight: FontWeight.bold
+
+              InkWell(
+                splashColor: Colors.red,
+                  onDoubleTap: (){
+                    print('duble clicked');
+                  },
+                  onLongPress: (){
+                    print('long clicked');
+                  },
+                  onTap: (){
+                    print('photo clicked');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('inkwell button'),
+                  )
               ),
-            ))
-          ],
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Phone',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    )
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration:(InputDecoration(
+                    hintText: 'password',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                    )
+                  )),
+                ),
+              ),
+
+
+              Card(
+                elevation: 5,
+               shadowColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Bobo card okay'),
+                ),
+              )
+            ],
+            
+          ),
         ),
       ),
     );
